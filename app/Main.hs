19f7c8d080578f500226585fc0e53d4287c167c5
Matt McFarland <783024+vanetix@users.noreply.github.com>
2020-03-09 14:51:31 -0500

    feat: add version command

--- app/Main.hs
+++ app/Main.hs
@@ -5,7 +5,9 @@ module Main where
 import Data.Either (fromRight)
 import Data.Time.Clock (getCurrentTime)
 import Data.Time.LocalTime (getCurrentTimeZone)
+import Data.Version (showVersion)
 import Options.Applicative
+import Paths_que (version)
 import qualified Que
 
 data Command
@@ -13,6 +15,7 @@ data Command
   | List
   | Done Int
   | Drop Int
+  | Version
 
 data Options =
   Options Command
@@ -23,10 +26,14 @@ main =
   execParser
     (info (helper <*> parseOptions) $
      fullDesc <>
-     progDesc "A small utility for managing todos" <> header "que - a local task manager")
+     progDesc "A small utility for managing todos" <>
+     header "que - a local task manager")
 
 parseOptions :: Parser Options
 parseOptions = Options <$> parseCommand
+
+parseVersion :: Parser Command
+parseVersion = pure Version
 
 parseNew :: Parser Command
 parseNew = New <$> strArgument (metavar "\"CONTENT\"")
@@ -43,12 +50,15 @@ parseDrop = Drop <$> argument auto (metavar "ID")
 parseCommand :: Parser Command
 parseCommand =
   subparser $
-  command "new"  (info parseNew $ progDesc "create a new item") <>
+  command "new" (info parseNew $ progDesc "create a new item") <>
   command "list" (info parseList $ progDesc "list all items") <>
   command "done" (info parseDone $ progDesc "complete an item") <>
-  command "drop" (info parseDrop $ progDesc "drop an item by id")
+  command "drop" (info parseDrop $ progDesc "drop an item by id") <>
+  command "version" (info parseVersion $ progDesc "print version and exit")
 
 run :: Options -> IO ()
+run (Options Version) = do
+  putStrLn $ showVersion version
 run (Options cmd) = do
   tz <- getCurrentTimeZone
   loadedTodos <- Que.load
@@ -59,18 +69,14 @@ run (Options cmd) = do
 runCommand :: Command -> Que.Todos -> IO Que.Todos
 runCommand cmd todos = do
   t <- getCurrentTime
-
   case cmd of
-    New content ->
-      pure $ Que.new t content todos
+    New content -> pure $ Que.new t content todos
     Done i ->
       case Que.done t i todos of
         Left msg -> do
           putStrLn msg
           pure $ todos
-        Right newTodos ->
-          pure $ newTodos
+        Right newTodos -> pure $ newTodos
     Drop i -> do
       pure $ Que.remove i todos
-    _ ->
-      pure todos
+    _ -> pure todos
