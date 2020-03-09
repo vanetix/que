a94cfbf0aa053ec85e01daa49eec09f862f19b15
Matt McFarland <783024+vanetix@users.noreply.github.com>
2020-03-09 15:10:44 -0500

    fix: fix broken tests after module rename

--- test/QueSpec.hs
+++ test/QueSpec.hs
@@ -1,9 +1,12 @@
-module LibSpec (main, spec) where
+module QueSpec
+  ( main
+  , spec
+  ) where
 
-import Test.Hspec
 import Data.Time.Clock (getCurrentTime)
+import Test.Hspec
 
-import qualified Lib
+import qualified Que
 
 main :: IO ()
 main = hspec spec
@@ -14,20 +17,18 @@ spec = do
     it "should create Todo" $ do
       todos <- new
       length todos `shouldBe` 1
-
   describe "done" $ do
     it "should mark todo complete" $ do
       t <- getCurrentTime
       todos <- new
-      let Right (Lib.Todo { Lib.completed = Just done } : []) = Lib.done t 1 todos
+      let Right (Que.Todo {Que.completed = Just done}:[]) = Que.done t 1 todos
       done `shouldBe` t
-
   describe "remove" $ do
     it "should remove todo" $ do
       todos <- new
-      (length $ Lib.remove 1 todos) `shouldBe` 0
+      (length $ Que.remove 1 todos) `shouldBe` 0
 
-new :: IO Lib.Todos
+new :: IO Que.Todos
 new = do
   t <- getCurrentTime
-  return $ Lib.new t "work" []
+  return $ Que.new t "work" []
